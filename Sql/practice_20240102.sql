bafe998665a0b2ab1bea5807815061d31e8786e4
Guilherme Cadori <61103271+guilhermecadori@users.noreply.github.com>
2024-10-02 18:58:44 -0300

    New: ex 4

--- Sql/practice_20240102.sql
+++ Sql/practice_20240102.sql
@@ -4,7 +4,13 @@ FROM users
 WHERE age >= 18
 
 -- Ex 2
-SELECT number1 % number2 as mod
+SELECT number1 % number2 AS mod
 FROM decimals
 
 -- Ex 3
+-- you are given a table 'otherangle' with columns 'a' and 'b'.
+-- return a table with these columns and your result in a column named 'res'.
+SELECT a, b, (180 - a - b) AS res
+FROM otherangle
+
+-- Ex 4
